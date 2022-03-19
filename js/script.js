7f11ff55a249f8450033dd7b4adf1cb79ab9565a
lizth93 <lizth93@hotmail.com>
2022-03-19 15:07:30 -0500

    modificationStyles

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const allLinks = document.querySelectorAll("a:link", function () {});
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
-    e.preventDefault();
+    // e.preventDefault();
     const href = link.getAttribute("href");
 
     if (href === "#")
